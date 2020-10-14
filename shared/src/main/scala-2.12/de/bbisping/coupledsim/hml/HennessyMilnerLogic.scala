e56a779de058b0d9f8bf42d774d82142253752cc
benkeks <benjamin.bisping@tu-berlin.de>
2020-10-15 00:15:18 +0200

    Remove redundant counting of negated deep branches

--- shared/src/main/scala-2.12/de/bbisping/coupledsim/hml/HennessyMilnerLogic.scala
+++ shared/src/main/scala-2.12/de/bbisping/coupledsim/hml/HennessyMilnerLogic.scala
@@ -11,8 +11,6 @@ object HennessyMilnerLogic {
     negationLevels: Int,
     /** the maximal amount of positive deep branches */
     maxPositiveDeepBranches: Int,
-    /** the maximal amount of negative deep branches */
-    maxNegativeDeepBranches: Int,
     /** the maximal amount of positive flat branches (height > 1); if all branches are flat, one positive branch will be counted as deep */
     maxPositiveFlatBranches: Int,
     /** maximal height of negative subformulas */
@@ -25,7 +23,6 @@ object HennessyMilnerLogic {
       Integer.max(this.conjunctionLevels, that.conjunctionLevels),
       Integer.max(this.negationLevels, that.negationLevels),
       Integer.max(this.maxPositiveDeepBranches, that.maxPositiveDeepBranches),
-      Integer.max(this.maxNegativeDeepBranches, that.maxNegativeDeepBranches),
       Integer.max(this.maxPositiveFlatBranches, that.maxPositiveFlatBranches),
       Integer.max(this.maxNegationHeight, that.maxNegationHeight),
       this.nonNegativeConjuncts || that.nonNegativeConjuncts
@@ -36,7 +33,6 @@ object HennessyMilnerLogic {
       Integer.min(this.conjunctionLevels, that.conjunctionLevels),
       Integer.min(this.negationLevels, that.negationLevels),
       Integer.min(this.maxPositiveDeepBranches, that.maxPositiveDeepBranches),
-      Integer.min(this.maxNegativeDeepBranches, that.maxNegativeDeepBranches),
       Integer.min(this.maxPositiveFlatBranches, that.maxPositiveFlatBranches),
       Integer.min(this.maxNegationHeight, that.maxNegationHeight),
       this.nonNegativeConjuncts && that.nonNegativeConjuncts
@@ -48,7 +44,6 @@ object HennessyMilnerLogic {
       this.conjunctionLevels >= that.conjunctionLevels &&
       this.negationLevels >= that.negationLevels &&
       this.maxPositiveDeepBranches >= that.maxPositiveDeepBranches &&
-      this.maxNegativeDeepBranches >= that.maxNegativeDeepBranches &&
       this.maxPositiveFlatBranches >= that.maxPositiveFlatBranches &&
       this.maxNegationHeight >= that.maxNegationHeight &&
       (this.nonNegativeConjuncts || !that.nonNegativeConjuncts)
@@ -59,7 +54,6 @@ object HennessyMilnerLogic {
       this.conjunctionLevels <= that.conjunctionLevels &&
       this.negationLevels <= that.negationLevels &&
       this.maxPositiveDeepBranches <= that.maxPositiveDeepBranches &&
-      this.maxNegativeDeepBranches <= that.maxNegativeDeepBranches &&
       this.maxPositiveFlatBranches <= that.maxPositiveFlatBranches &&
       this.maxNegationHeight <= that.maxNegationHeight &&
       (!this.nonNegativeConjuncts || that.nonNegativeConjuncts)
@@ -70,17 +64,17 @@ object HennessyMilnerLogic {
   // height, conjunctionLevels, negationLevels, maxPosDeep, maxNegDeep, maxPosFlat, maxNegH, nonNegConjs
   // nonNegConjs is necessary, because maxPosFlat will sometimes count one positive flat branch as deep to account for trace equivalences. 
   val ObservationClasses = List(
-    "traces" ->             ObservationClass(INFTY,     0,    0,    0,    0,    0,    0,false),
-    "failure" ->            ObservationClass(INFTY,     1,    1,    0,    0,    0,    1,false),
-    "readiness" ->          ObservationClass(INFTY,     1,    1,    0,    0,INFTY,    1,true),
-    "failure-trace" ->      ObservationClass(INFTY, INFTY,    1,    1,    0,    0,    1,true),
-    "ready-trace" ->        ObservationClass(INFTY, INFTY,    1,    1,    0,INFTY,    1,true),
-    "impossible-future" ->  ObservationClass(INFTY,     1,    1,    0,INFTY,    0,INFTY,false),
-    "possible-future" ->    ObservationClass(INFTY,     1,    1,INFTY,INFTY,INFTY,INFTY,true),
-    "simulation" ->         ObservationClass(INFTY, INFTY,    0,INFTY,    0,INFTY,    0,true),
-    "ready-simulation" ->   ObservationClass(INFTY, INFTY,    1,INFTY,    0,INFTY,    1,true),
-    "2-nested-simulation"-> ObservationClass(INFTY, INFTY,    1,INFTY,INFTY,INFTY,INFTY,true),
-    "bisimulation" ->       ObservationClass(INFTY, INFTY,INFTY,INFTY,INFTY,INFTY,INFTY,true)
+    "traces" ->             ObservationClass(INFTY,     0,    0,    0,    0,    0,false),
+    "failure" ->            ObservationClass(INFTY,     1,    1,    0,    0,    1,false),
+    "readiness" ->          ObservationClass(INFTY,     1,    1,    0,INFTY,    1,true),
+    "failure-trace" ->      ObservationClass(INFTY, INFTY,    1,    1,    0,    1,true),
+    "ready-trace" ->        ObservationClass(INFTY, INFTY,    1,    1,INFTY,    1,true),
+    "impossible-future" ->  ObservationClass(INFTY,     1,    1,    0,    0,INFTY,false),
+    "possible-future" ->    ObservationClass(INFTY,     1,    1,INFTY,INFTY,INFTY,true),
+    "simulation" ->         ObservationClass(INFTY, INFTY,    0,INFTY,INFTY,    0,true),
+    "ready-simulation" ->   ObservationClass(INFTY, INFTY,    1,INFTY,INFTY,    1,true),
+    "2-nested-simulation"-> ObservationClass(INFTY, INFTY,    1,INFTY,INFTY,INFTY,true),
+    "bisimulation" ->       ObservationClass(INFTY, INFTY,INFTY,INFTY,INFTY,INFTY,true)
   )
 
   def getLeastDistinguishing[A](formulas: Set[Formula[A]]) = {
@@ -108,7 +102,6 @@ object HennessyMilnerLogic {
         obsClass.conjunctionLevels + (if (!isPositive) 1 else 0),
         obsClass.negationLevels,
         obsClass.maxPositiveDeepBranches,
-        Integer.max(obsClass.maxNegativeDeepBranches, (if (!isPositive && obsClass.height > 2) 1 else 0)),
         obsClass.maxPositiveFlatBranches,
         obsClass.maxNegationHeight,
         obsClass.nonNegativeConjuncts
@@ -151,7 +144,7 @@ object HennessyMilnerLogic {
     override val obsClass = {
       
       if (subterms.isEmpty) {
-        ObservationClass(0,0,0,0,0,0,0,false)
+        ObservationClass(0,0,0,0,0,0,false)
       } else {
         val (deepSubtermsPrelim, flatSubtermsPrelim) = subterms.partition(_.obsClass.height > 1)
         val positiveFlat = flatSubtermsPrelim.find(_.isPositive)
@@ -169,8 +162,6 @@ object HennessyMilnerLogic {
           negationLevels = subterms.map(_.obsClass.negationLevels).max,
           /** the maximal amount of positive deep branches */
           maxPositiveDeepBranches = (subterms.map(_.obsClass.maxPositiveDeepBranches) + deepSubtermsPrelim.count(_.isPositive)).max,
-          /** the maximal amount of negative deep branches */
-          maxNegativeDeepBranches  = (subterms.map(_.obsClass.maxNegativeDeepBranches) + deepSubtermsPrelim.count(f => !f.isPositive)).max,
           /** the maximal amount of positive flat branches (height > 1); if all branches are flat, one positive branch will be exempted from the count */
           maxPositiveFlatBranches = (subterms.map(_.obsClass.maxPositiveFlatBranches) + flatSubterms.count(f => f.isPositive)).max,
           /** maximal height of negative subformulas */
@@ -186,7 +177,7 @@ object HennessyMilnerLogic {
 
     override val isPositive = true
     
-    override val obsClass = ObservationClass(andThen.obsClass.height + 1, andThen.obsClass.conjunctionLevels + (if (!andThen.isPositive) 1 else 0),0,0,0,0,0, false) lub andThen.obsClass
+    override val obsClass = ObservationClass(andThen.obsClass.height + 1, andThen.obsClass.conjunctionLevels + (if (!andThen.isPositive) 1 else 0),0,0,0,0, false) lub andThen.obsClass
 
   }
 
@@ -195,7 +186,7 @@ object HennessyMilnerLogic {
 
     override val isPositive = false
 
-    override val obsClass = ObservationClass(0, andThen.obsClass.conjunctionLevels + (if (!andThen.isPositive) 1 else 0),andThen.obsClass.negationLevels + 1,0,0,0,andThen.obsClass.height,false) lub andThen.obsClass
+    override val obsClass = ObservationClass(0, andThen.obsClass.conjunctionLevels + (if (!andThen.isPositive) 1 else 0),andThen.obsClass.negationLevels + 1,0,0,andThen.obsClass.height,false) lub andThen.obsClass
 
   }
 
