6205797c69d33ec18a603d0f617b6f8fc8fe4bc6
benkeks <benjamin.bisping@tu-berlin.de>
2022-01-18 13:16:25 +0100

    Add automated tests for the current counter examples

--- shared/src/test/scala-2.12/de/bbisping/coupledsim/LTBTSTests.scala
+++ shared/src/test/scala-2.12/de/bbisping/coupledsim/LTBTSTests.scala
@@ -1,30 +1,10 @@
 package de.bbisping.coupledsim
 
-import org.scalatest.funspec.AnyFunSpec
-import org.scalatest.Inspectors.forAll
-import org.scalatest.matchers.BeMatcher
-import org.scalatest.matchers.should
 
-import de.bbisping.coupledsim.tool.model.NodeID
-import de.bbisping.coupledsim.util.Relation
-import de.bbisping.coupledsim.hml.ObservationClass
-import de.bbisping.coupledsim.hml.HMLGamePlayer
-import de.bbisping.coupledsim.algo.AlgorithmLogging
-import de.bbisping.coupledsim.algo.AlgorithmLogging.{LogRelation, LogRichRelation}
-
-class LTBTSTests extends AnyFunSpec with should.Matchers  {
-  
-  AlgorithmLogging.loggingActive = true
-  AlgorithmLogging.debugLogActive = false
-  
+class LTBTSTests extends CSSSampleTests {
   val ltbtsSystem = TestSamples.samples.find(_._1 == "ltbts1").get._2
 
-  private def toSpectrumClassSet(names: Iterable[String]) = (for {
-    n <- names
-    cl <- ObservationClass.getSpectrumClass(n)
-  } yield cl).toSet
-
-  val samples = List(
+  val sampleNames: List[(String, String, List[String], List[String])] = List(
     ("L13", "R13", List("simulation"), List("failure")),
     ("R13", "L13", List("2-nested-simulation"), List("bisimulation")),
     ("L16", "R16", List("simulation"), List("failure")),
@@ -45,46 +25,7 @@ class LTBTSTests extends AnyFunSpec with should.Matchers  {
     ("R42", "L42", List("2-nested-simulation"), List("bisimulation")),
     ("L50", "R50", List("ready-simulation"), List("impossible-future")),
     ("R50", "L50", List("ready-trace", "impossible-future"), List("possible-future", "simulation"))
-  ).map {
-    case (n1, n2, preords, notPreords) =>
-      (n1, n2, toSpectrumClassSet(preords), toSpectrumClassSet(notPreords))
-  }
-  
-  describe("The Spectroscopy") {
-    forAll(samples) { case (n1s, n2s, preords, notPreords) =>
-      describe("for " + n1s + " <= " + n2s) {
-        val n1 = NodeID(n1s)
-        val n2 = NodeID(n2s)
-        val preordsStr = preords.map(_._1)
-        val notPreordsStr = notPreords.map(_._1)
+  )
 
-        val algo = new HMLGamePlayer(ltbtsSystem, List(n1, n2))
-        algo.compute()
-
-        val log = for {
-          r <- algo.getReplay()
-        } yield r()
-
-        var foundPreorders = Set[String]()
-
-        log.foreach {
-          case LogRichRelation(_, comment) if comment.startsWith(n1 + " distinguished") =>
-            val commentParts = comment.split(Array(',', ' ')).toSet intersect ObservationClass.LTBTSNotionNames
-            val diff = notPreordsStr diff commentParts
-            it ("should be distinguished by " + notPreordsStr.mkString(",")) {
-              diff should be (empty)
-            }
-          case LogRelation(_, comment) if comment.startsWith(n1 + " preordered") =>
-            val commentParts = comment.split(Array(',', ' ')).toSet intersect ObservationClass.LTBTSNotionNames
-            foundPreorders ++= commentParts
-          case _ =>
-        }
-
-        it ("should only be preordered by " + preordsStr.mkString(",")) {
-          (foundPreorders diff preordsStr) should be (empty)
-          (preordsStr diff foundPreorders) should be (empty)
-        }
-      }
-    }
-  }
+  runTest(ltbtsSystem, sampleNames)
 }
