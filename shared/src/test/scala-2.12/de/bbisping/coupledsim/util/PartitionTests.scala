32702b0e9e096c89c4176a02d488ebc8c73432ca
benkeks <benjamin.bisping@tu-berlin.de>
2022-01-19 21:09:23 +0100

    Add tests for partition enumeration

---
+++ shared/src/test/scala-2.12/de/bbisping/coupledsim/util/PartitionTests.scala
@@ -0,0 +1,22 @@
+package de.bbisping.coupledsim.util
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should
+
+class PartitionTests extends AnyFunSpec with should.Matchers  {
+  
+  describe("Listing Partitions") {
+    it("should yield the trivial partition for one element") {
+      Partition.partitioningListsOfSet(Set(1)) should contain theSameElementsAs List(List(Set(1)))
+    }
+    it("should yield a binary and a trivial partiion for two elements") {
+      Partition.partitioningListsOfSet(Set(1,2)).map(_.toSet) should contain theSameElementsAs List(Set(Set(1),Set(2)), Set(Set(1,2)))
+    }
+    it("should yield five permutations for three elements") {
+      Partition.partitioningListsOfSet(Set(1,2,3)).map(_.toSet) should contain theSameElementsAs List(
+        Set(Set(1),Set(2),Set(3)),
+        Set(Set(1,2),Set(3)), Set(Set(1,3),Set(2)), Set(Set(3,2),Set(1)),
+        Set(Set(1,2,3)))
+    }
+  }
+}
