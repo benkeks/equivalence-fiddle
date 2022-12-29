6384b2f28728a19bb2f5e2f110d9d307b2fc2c35
benkeks <benjamin.bisping@tu-berlin.de>
2022-12-29 13:23:18 +0100

    Align metric with existing ones and add defender check nodes

--- shared/src/main/scala-2.12/io/equiv/eqfiddle/hml/ObservationClassFast.scala
+++ shared/src/main/scala-2.12/io/equiv/eqfiddle/hml/ObservationClassFast.scala
@@ -5,14 +5,14 @@ case class ObservationClassFast(
   observationHeight: Int = 0,
   /** the maximal amount of conjunctions when descending into a formula */
   conjunctionLevels: Int = 0,
-  /** the maximal height of failure-test conjunctions (observationHeight)*/
-  maxFailureHeight: Int = 0,
-  /** the maximal height of readiness-test conjunctions (observationHeight)*/
-  maxReadinessHeight: Int = 0,
-  /** how many deep negative conjunct levels may happen? */
-  deepNegativeConjunctionLevels: Int = 0,
-  /** how many deep positive conjunct levels may happen? */
-  deepPositiveConjunctionLevels: Int = 0
+  /** the maximal height of revivals at local-test conjunctions (observationHeight)*/
+  revivalHeight: Int = 0,
+  /** maximal height of positive conjunctions (observationHeight) excluding revivals*/
+  positiveConjHeight: Int = 0,
+  /** maximal height of negative conjuncts */
+  negativeConjHeight: Int = 0,
+  /** how many negation levels may happen? */
+  negationLevels: Int = 0
 ) extends ObservationClass {
 
   override def tryCompareTo[B >: ObservationClass](that: B)(implicit evidence$1: B => PartiallyOrdered[B]): Option[Int] = {
@@ -23,18 +23,18 @@ case class ObservationClassFast(
         } else if (
             this.observationHeight >= that.observationHeight &&
             this.conjunctionLevels >= that.conjunctionLevels &&
-            this.maxFailureHeight >= that.maxFailureHeight &&
-            this.maxReadinessHeight >= that.maxReadinessHeight &&
-            this.deepNegativeConjunctionLevels >= that.deepNegativeConjunctionLevels &&
-            this.deepPositiveConjunctionLevels >= that.deepPositiveConjunctionLevels) {
+            this.revivalHeight >= that.revivalHeight &&
+            this.positiveConjHeight >= that.positiveConjHeight &&
+            this.negativeConjHeight >= that.negativeConjHeight &&
+            this.negationLevels >= that.negationLevels) {
           Some(1)
         } else if (
             this.observationHeight <= that.observationHeight &&
             this.conjunctionLevels <= that.conjunctionLevels &&
-            this.maxFailureHeight <= that.maxFailureHeight &&
-            this.maxReadinessHeight <= that.maxReadinessHeight &&
-            this.deepNegativeConjunctionLevels <= that.deepNegativeConjunctionLevels &&
-            this.deepPositiveConjunctionLevels <= that.deepPositiveConjunctionLevels) {
+            this.revivalHeight <= that.revivalHeight &&
+            this.positiveConjHeight <= that.positiveConjHeight &&
+            this.negativeConjHeight <= that.negativeConjHeight &&
+            this.negationLevels <= that.negationLevels) {
           Some(-1)
         } else {
           None
@@ -48,10 +48,10 @@ case class ObservationClassFast(
       ObservationClassFast(
         Integer.max(this.observationHeight, that.observationHeight),
         Integer.max(this.conjunctionLevels, that.conjunctionLevels),
-        Integer.max(this.maxFailureHeight, that.maxFailureHeight),
-        Integer.max(this.maxReadinessHeight, that.maxReadinessHeight),
-        Integer.max(this.deepNegativeConjunctionLevels, that.deepNegativeConjunctionLevels),
-        Integer.max(this.deepPositiveConjunctionLevels, that.deepPositiveConjunctionLevels)
+        Integer.max(this.revivalHeight, that.revivalHeight),
+        Integer.max(this.positiveConjHeight, that.positiveConjHeight),
+        Integer.max(this.negativeConjHeight, that.negativeConjHeight),
+        Integer.max(this.negationLevels, that.negationLevels)
       )
     case _ => this
   }
@@ -61,34 +61,34 @@ case class ObservationClassFast(
       ObservationClassFast(
         Integer.min(this.observationHeight, that.observationHeight),
         Integer.min(this.conjunctionLevels, that.conjunctionLevels),
-        Integer.min(this.maxFailureHeight, that.maxFailureHeight),
-        Integer.min(this.maxReadinessHeight, that.maxReadinessHeight),
-        Integer.min(this.deepNegativeConjunctionLevels, that.deepNegativeConjunctionLevels),
-        Integer.min(this.deepPositiveConjunctionLevels, that.deepPositiveConjunctionLevels)
+        Integer.min(this.revivalHeight, that.revivalHeight),
+        Integer.min(this.positiveConjHeight, that.positiveConjHeight),
+        Integer.min(this.negativeConjHeight, that.negativeConjHeight),
+        Integer.min(this.negationLevels, that.negationLevels)
       )
     case _ => this
   }
 
-  override def toTuple = (observationHeight, conjunctionLevels, maxFailureHeight, maxReadinessHeight, deepNegativeConjunctionLevels, deepPositiveConjunctionLevels)
+  override def toTuple = (observationHeight, conjunctionLevels, revivalHeight, positiveConjHeight, negativeConjHeight, negationLevels)
 }
 
 object ObservationClassFast {
   val INFTY = Integer.MAX_VALUE
 
-  // observationHeight, conjunctionLevels, maxFailureHeight, maxReadinessHeight, deepNegativeConjunctionLevels, deepPositiveConjunctionLevels
+  // observationHeight, conjunctionLevels, revivalHeight, positiveConjHeight, negativeConjHeight, negationLevels
   // The Linear-time Branching-time Spectrum
   val BaseLTBTS = List(
     "enabledness" ->        ObservationClassFast(    1,     0,    0,    0,    0,    0),
     "traces" ->             ObservationClassFast(INFTY,     0,    0,    0,    0,    0),
-    "failure" ->            ObservationClassFast(INFTY,     1,    1,    0,    0,    0),
-    "readiness" ->          ObservationClassFast(INFTY,     1,    1,    1,    0,    0),
-    "failure-trace" ->      ObservationClassFast(INFTY, INFTY,INFTY,    0,    0,    0),
-    "ready-trace" ->        ObservationClassFast(INFTY, INFTY,INFTY,INFTY,    0,    0),
-    "impossible-future" ->  ObservationClassFast(INFTY,     1,    1,    0,    1,    0),
-    "possible-future" ->    ObservationClassFast(INFTY,     1,INFTY,INFTY,    1,    1),
-    "simulation" ->         ObservationClassFast(INFTY, INFTY,    0,INFTY,    0,INFTY),
-    "ready-simulation" ->   ObservationClassFast(INFTY, INFTY,INFTY,INFTY,    0,INFTY),
-    "2-nested-simulation"-> ObservationClassFast(INFTY, INFTY,INFTY,INFTY,    1,INFTY), // TODO: Double spending problem of negations!
+    "failure" ->            ObservationClassFast(INFTY,     1,    0,    0,    1,    1),
+    "readiness" ->          ObservationClassFast(INFTY,     1,    1,    1,    1,    1),
+    "failure-trace" ->      ObservationClassFast(INFTY, INFTY,INFTY,    0,    1,    1),
+    "ready-trace" ->        ObservationClassFast(INFTY, INFTY,INFTY,    1,    1,    1),
+    "impossible-future" ->  ObservationClassFast(INFTY,     1,    0,    0,INFTY,    1),
+    "possible-future" ->    ObservationClassFast(INFTY,     1,INFTY,INFTY,INFTY,    1),
+    "simulation" ->         ObservationClassFast(INFTY, INFTY,INFTY,INFTY,    0,    0),
+    "ready-simulation" ->   ObservationClassFast(INFTY, INFTY,INFTY,INFTY,    1,    1),
+    "2-nested-simulation"-> ObservationClassFast(INFTY, INFTY,INFTY,INFTY,INFTY,    1),
     "bisimulation" ->       ObservationClassFast(INFTY, INFTY,INFTY,INFTY,INFTY,INFTY)
   )
 
@@ -109,30 +109,31 @@ object ObservationClassFast {
           ObservationClassFast(
             observationHeight = allClasses.map(_.observationHeight).max,
             conjunctionLevels = allClasses.map(_.conjunctionLevels).max + 1,
-            maxFailureHeight = allClasses.map(_.maxFailureHeight).max,
-            maxReadinessHeight = allClasses.map(_.maxReadinessHeight).max,
-            deepNegativeConjunctionLevels =
-              allClasses.map(_.deepNegativeConjunctionLevels).max + (if (negativeFlat.nonEmpty || negativeDeep.nonEmpty) 1 else 0),
-            deepPositiveConjunctionLevels =
-              allClasses.map(_.deepPositiveConjunctionLevels).max + (if (positiveFlat.nonEmpty || positiveDeep.nonEmpty) 1 else 0),
+            revivalHeight = allClasses.map(_.revivalHeight).max,
+            positiveConjHeight = (allClasses.map(_.positiveConjHeight) ++ (positiveFlat ++ positiveDeep).map(_.observationHeight)).max,
+            negativeConjHeight = (allClasses.map(_.negativeConjHeight) ++ (negativeFlat ++ negativeDeep).map(_.observationHeight)).max,
+            negationLevels = allClasses.map(_.negationLevels).max,
           )
         } else {
           // this conjunction can be understood as a local observation
-          val revivalDepth = (positiveDeep.headOption).map(_.observationHeight).getOrElse(1)
+          val revivalDepth = (positiveDeep.headOption orElse positiveFlat.headOption).map(_.observationHeight).getOrElse(0)
           ObservationClassFast(
             observationHeight = allClasses.map(_.observationHeight).max,
             conjunctionLevels = allClasses.map(_.conjunctionLevels).max + 1,
-            maxFailureHeight =
-              if (negativeFlat.nonEmpty) Integer.max(revivalDepth, allClasses.map(_.maxFailureHeight).max) else allClasses.map(_.maxFailureHeight).max,
-            maxReadinessHeight =
-              if (positiveFlat.nonEmpty) Integer.max(revivalDepth, allClasses.map(_.maxReadinessHeight).max) else allClasses.map(_.maxReadinessHeight).max,
-            deepNegativeConjunctionLevels = allClasses.map(_.deepNegativeConjunctionLevels).max,
-            deepPositiveConjunctionLevels = allClasses.map(_.deepPositiveConjunctionLevels).max,
+            revivalHeight = Integer.max(revivalDepth, allClasses.map(_.revivalHeight).max),
+            positiveConjHeight =
+              if (positiveFlat.nonEmpty) Integer.max(1, allClasses.map(_.positiveConjHeight).max) else allClasses.map(_.positiveConjHeight).max,
+            negativeConjHeight =
+              if (negativeFlat.nonEmpty) Integer.max(1, allClasses.map(_.negativeConjHeight).max) else allClasses.map(_.negativeConjHeight).max,
+            negationLevels = allClasses.map(_.negationLevels).max,
           )
         }
       }
     case HennessyMilnerLogic.Negate(andThen) =>
-      formulaObsClass(andThen)
+      val andThenClass = formulaObsClass(andThen)
+      ObservationClassFast(
+        negationLevels = andThenClass.negationLevels + 1
+      ) lub andThenClass
     case HennessyMilnerLogic.Observe(action, andThen) =>
       val andThenClass = formulaObsClass(andThen)
       ObservationClassFast(
--- shared/src/main/scala-2.12/io/equiv/eqfiddle/spectroscopy/EnergySpectroscopyGame.scala
+++ shared/src/main/scala-2.12/io/equiv/eqfiddle/spectroscopy/EnergySpectroscopyGame.scala
@@ -11,10 +11,12 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
   private val NoEnergyUpdate        = new EnergyGame.EnergyUpdate(Array( 0,0,0,0,0,0), energyCap = energyCap)
   private val ObsEnergyUpdate       = new EnergyGame.EnergyUpdate(Array(-1,0,0,0,0,0), energyCap = energyCap)
   private val ConjEnergyUpdate      = new EnergyGame.EnergyUpdate(Array(0,-1,0,0,0,0), energyCap = energyCap)
-  private val FailureEnergyUpdate   = new EnergyGame.EnergyUpdate(Array(3,-1,0,0,0,0), energyCap = energyCap)
-  private val ReadinessEnergyUpdate = new EnergyGame.EnergyUpdate(Array( 4,0,0,0,0,0), energyCap = energyCap)
-  private val NegClauseEnergyUpdate = new EnergyGame.EnergyUpdate(Array(0,0,0,0,-1,0), energyCap = energyCap)
-  private val PosClauseEnergyUpdate = new EnergyGame.EnergyUpdate(Array(0,0,0,0,0,-1), energyCap = energyCap)
+  private val LocalObsEnergyUpdate  = new EnergyGame.EnergyUpdate(Array(-1,-1,0,0,0,0), energyCap = energyCap)
+  private val TestFailureEnergyUpdate = new EnergyGame.EnergyUpdate(Array(0,0,0,0,-1,-1), energyCap = energyCap)
+  private val TestReadinessEnergyUpdate = new EnergyGame.EnergyUpdate(Array(0,0,0,-1,0,0), energyCap = energyCap)
+  private val RevivalEnergyUpdate   = new EnergyGame.EnergyUpdate(Array(3,0,0,0,0,0), energyCap = energyCap)
+  private val NegClauseEnergyUpdate = new EnergyGame.EnergyUpdate(Array(5,0,0,0,0,-1), energyCap = energyCap)
+  private val PosClauseEnergyUpdate = new EnergyGame.EnergyUpdate(Array(4,0,0,0,0,0), energyCap = energyCap)
 
   /* This will abort the game construction in nodes where the attacker cannot win because p is contained in qq. */
   val optimizeSymmetryDefWins: Boolean = true
@@ -24,25 +26,20 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
   case class AttackerClause(p: S, q: S) extends SimpleGame.AttackerNode
   case class AttackerLocalObservation(p: S, qq: Set[S]) extends SimpleGame.AttackerNode
   case class DefenderConjunction(p: S, qq: Set[S]) extends SimpleGame.DefenderNode
-  
+  case class DefenderTestFailureObservability(p: S, qq: Set[S]) extends SimpleGame.DefenderNode
+  case class DefenderTestReadinessObservability(p: S, qq: Set[S]) extends SimpleGame.DefenderNode
+
   override def weight(gn1: GameNode, gn2: GameNode): EnergyGame.EnergyUpdate = gn1 match {
     case AttackerObservation(p0, qq0) =>
       gn2 match {
         case AttackerObservation(p1, qq1) =>
           ObsEnergyUpdate
-        case AttackerLocalObservation(p1, qq1) =>
-          FailureEnergyUpdate
+        case DefenderTestFailureObservability(p1, qq1) =>
+          LocalObsEnergyUpdate
+        case DefenderTestReadinessObservability(p1, qq1) =>
+          LocalObsEnergyUpdate
         case DefenderConjunction(p1, qq1) =>
           ConjEnergyUpdate
-      }
-    case AttackerLocalObservation(p0, qq0) =>
-      gn2 match {
-        case AttackerObservation(p1, qq1) =>
-          ObsEnergyUpdate
-        case AttackerLocalObservation(p1, qq1) =>
-          ReadinessEnergyUpdate
-        case DefenderConjunction(p1, qq1) =>
-          ObsEnergyUpdate
       }
     case AttackerClause(p0, q0) =>
       gn2 match {
@@ -50,6 +47,20 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
           PosClauseEnergyUpdate
         case AttackerObservation(p1, qq1) if qq1 contains p0 =>
           NegClauseEnergyUpdate
+      }
+    case DefenderTestFailureObservability(p0, qq0) =>
+      gn2 match {
+        case AttackerLocalObservation(p1, qq1) =>
+          RevivalEnergyUpdate
+        case DefenderConjunction(p1, qq1) =>
+          TestFailureEnergyUpdate
+      }
+    case DefenderTestReadinessObservability(p0, qq0) =>
+      gn2 match {
+        case AttackerLocalObservation(p1, qq1) =>
+          RevivalEnergyUpdate
+        case DefenderConjunction(p1, qq1) =>
+          TestReadinessEnergyUpdate
       }
     case _ =>
       NoEnergyUpdate
@@ -65,7 +76,10 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
           // prioritize instant wins because of stuck defender
           List(conjMove)
         } else {
-          val failMove = AttackerLocalObservation(p0, qq0.filter(q0 => ts.enabled(q0) subsetOf ts.enabled(p0)))
+          val failChallengedQ = qq0.filter(q0 => ts.enabled(q0) subsetOf ts.enabled(p0))
+          val failMove = if (failChallengedQ.size < qq0.size) List(DefenderTestFailureObservability(p0, failChallengedQ)) else List()
+          val readyChallengedQ = qq0.filter(q0 => ts.enabled(p0) subsetOf ts.enabled(q0))
+          val readyMove = if (readyChallengedQ.size < qq0.size) List(DefenderTestReadinessObservability(p0, readyChallengedQ)) else List()
           val dn = for {
             (a,pp1) <- ts.post(p0)
             p1 <- pp1
@@ -73,7 +87,7 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
           } yield {
             AttackerObservation(p1, qq1)
           }
-          dn ++ List(failMove, conjMove)
+          dn ++ failMove ++ readyMove ++ List(conjMove)
         }
       }
     case AttackerLocalObservation(p0, qq0) =>
@@ -84,7 +98,8 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
         if (qq0.isEmpty) {
           List(conjMove)
         } else {
-          val readyMove = AttackerLocalObservation(p0, qq0.filter(q0 => ts.enabled(p0) subsetOf ts.enabled(q0)))
+          val readyChallengedQ = qq0.filter(q0 => ts.enabled(p0) subsetOf ts.enabled(q0))
+          val readyMove = if (readyChallengedQ.size < qq0.size) List(DefenderTestReadinessObservability(p0, readyChallengedQ)) else List()
           val dn = for {
             (a,pp1) <- ts.post(p0)
             p1 <- pp1
@@ -92,7 +107,7 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
           } yield {
             AttackerObservation(p1, qq1)
           }
-          dn ++ List(readyMove)
+          dn ++ readyMove
         }
       }
     case AttackerClause(p0, q0) =>
@@ -105,5 +120,15 @@ class EnergySpectroscopyGame[S, A, L](ts: WeakTransitionSystem[S, A, L], energyC
       } yield {
         AttackerClause(p0, q1)
       }
+    case DefenderTestFailureObservability(p0, qq0) =>
+      List(
+        DefenderConjunction(p0, Set.empty),
+        AttackerLocalObservation(p0, qq0),
+      )
+    case DefenderTestReadinessObservability(p0, qq0) =>
+      List(
+        DefenderConjunction(p0, Set.empty),
+        AttackerLocalObservation(p0, qq0),
+      )
   }
 }
